58feb4144b2224f1dd80722f98d381d8b064bcf9
hthuz <hanggang.20@intl.zju.edu.cn>
2023-07-11 17:03:10 +0800

    Update: msedge shortcut

--- chadwm/config.def.h
+++ chadwm/config.def.h
@@ -151,6 +151,7 @@ static const Key keys[] = {
 
     { MODKEY,                           XK_c,       spawn,          SHCMD("rofi -show drun") },
     { MODKEY,                           XK_Return,  spawn,            SHCMD("st")},
+    { MODKEY,                           XK_e,       spawn,            SHCMD("microsoft-edge-stable")},
 
     // toggle stuff
     { MODKEY,                           XK_b,       togglebar,      {0} },
@@ -236,8 +237,8 @@ static const Key keys[] = {
     { MODKEY|ShiftMask,                 XK_r,       restart,           {0} },
 
     // hide & restore windows
-    { MODKEY,                           XK_e,       hidewin,        {0} },
-    { MODKEY|ShiftMask,                 XK_e,       restorewin,     {0} },
+    // { MODKEY,                           XK_e,       hidewin,        {0} },
+    // { MODKEY|ShiftMask,                 XK_e,       restorewin,     {0} },
 
     TAGKEYS(                            XK_1,                       0)
     TAGKEYS(                            XK_2,                       1)
--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 xrdb merge ~/.Xresources 
 xbacklight -set 10 &
 setxkbmap -option caps:escape
-feh --bg-fill ~/picture/wall2.jpg &
+feh --bg-fill ~/picture/wall3.jpg &
 xset r rate 200 50 &
 picom &
 
